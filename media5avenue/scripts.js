4c131d0c52e4f22f1bcd19fa60f220b7b02e44d1
twiin inno <ashleykarim108@gmail.com>
2025-07-26 12:37:20 +0300

    Add files via upload

---
+++ media5avenue/scripts.js
@@ -0,0 +1,5 @@
+// For search functionality (simple alert to show search term)
+document.getElementById('search').addEventListener('input', function () {
+    console.log('Searching for: ', this.value);
+  });
+  
